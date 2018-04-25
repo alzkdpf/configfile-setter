68287c8167d8b2974572e4bce206a39905da5bf6
Michael L <code21032@gmail.com>
2018-04-25 14:29:08 +0900

    update

--- download.sh
+++ download.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 user_name=$1
 
-if [ -z "$user_name" ]; then
-    echo "need argument required for user_name";
+if [ "$1" == "-h" ]; then
+    echo "./download.sh [user_name] [host] [port] [target_path] [destination_path]"
+    exit 0;
+elif [ -z "$user_name" ]; then
+    echo "-h show options";
     exit 0; #exit 0 정상 종료를 의미
 fi
 
-host=192.168.5.0
-target_path=/configuration/test/test.cfg
-destination_path=./
+host=$2
+port=$3
+target_path=$4
+destination_path=$5
 
-sftp -oPort=2929 $user_name@$host:$target_path $destination_path
+sftp -oPort=$port $user_name@$host:$target_path $destination_path
