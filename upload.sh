a92ce241c851ca9c0db73f3bf383730cca6cb468
Michael <code21032@gmail.com>
2018-04-26 21:46:26 +0900

    update uploader

--- upload.sh
+++ upload.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
-user_name=$1
+
+port=$1
+user_name=$2
+host=$3
+remote_path=$4
+local_file_path=$5
 
 if [ "$1" == "-h" ]; then
-  echo "Usage: `basename $0` [somestuff]"
+  echo "./upload.sh [port] [user_name] [host] [remote_path] [local_file_path]"
   exit 0
 elif [ -z "$user_name" ]; then
     echo "-h show options";
     exit 0; #exit 0 정상 종료를 의미
 fi
 
-remote_path=$2
-
 if [ -z "$remote_path" ]; then
     echo "need argument required for remote_path";
     exit 0; #exit 0 정상 종료를 의미
 fi
-
-host=192.168.5.0
-local_file_path=$3
 
 if [ -z "$local_file_path" ]; then
     echo "need argument required for local_file_path";
@@ -26,4 +26,4 @@ fi
 
 put=$(printf "put %s" $3)
 
-sftp -oPort=2929 $user_name@$host:$remote_path <<< $put
+sftp -oPort=$port $user_name@$host:$remote_path <<< $put
